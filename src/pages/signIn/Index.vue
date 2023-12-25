3b4ef672fcd7835eccdef20707726c0e34c45874
Adriann Guedes <adrianguedes902@gmail.com>
2023-12-25 10:04:00 -0300

    add route and file to signin

---
+++ src/pages/signIn/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page class="flex flex-center column">
+   <q-img src="../../../src/assets/instadev-logo.svg"/>
+   <q-input filled v-model="text" label="Filled" />
+   <q-input filled v-model="text" label="Filled" />
+   <a href="">Forgot password?</a>
+   <q-btn color="primary" label="Primary" />
+   <div>
+    <q-img src="../../../src/assets/facebook-logo.svg"/>
+    <a href="">Log in with Facebook</a>
+   </div>
+   <div class="full-width">
+    <q-separator/>
+      or
+    <q-separator/>
+   </div>
+   <div>
+    Don´t have an account? <a href="">Sign up</a>
+
+   </div>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'SignIn',
+  data() {
+    return {
+      text: '',
+    };
+  },
+};
+</script>
