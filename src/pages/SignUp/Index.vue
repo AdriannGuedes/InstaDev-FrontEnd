d7231adf21d68dab52e7911fd53ab2bfd10a5abc
Adriann Guedes <adrianguedes902@gmail.com>
2024-01-19 22:23:42 -0300

    create sign up and create post page

---
+++ src/pages/SignUp/Index.vue
@@ -0,0 +1,78 @@
+<template>
+  <q-page class="flex flex-center column justify-center q-px-md">
+    <q-icon
+     class="absolute-left q-ma-lg" name="fa-solid fa-chevron-left" size="25px" color="grey">
+    </q-icon>
+  <div class="full-width items-center column justify-center">
+   <q-img class="logo q-mb-lg" src="../../../src/assets/instadev-logo.svg"/>
+   <q-input filled v-model="email" label="E-mail" class="full-width q-mb-md"/>
+   <q-input filled v-model="userName" label="Username" type="password"  class="full-width q-mb-md"/>
+   <q-input filled v-model="password" label="Password" type="password"  class="full-width q-mb-md"/>
+   <q-input
+     filled
+     v-model="confirmPassword"
+     label="Confirm Password"
+     type="password"
+     class="full-width"
+   />
+
+   <q-btn
+     color="primary"
+     :disable="true"
+     label="Sign up"
+     class="sign-in-button full-width q-mt-lg"/>
+
+   <div class="full-width row items-center justify-center q-my-xl">
+    <q-separator class="separator" inset/>
+      OR
+    <q-separator class="separator" inset/>
+   </div>
+   <div class="full-width row items-center justify-center">
+    <span class=" text-opacity">already have an account?</span>
+    <a href="" class="link q-ml-xs">Sign In</a>
+    </div>
+  </div>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'SignUp',
+  data() {
+    return {
+      email: '',
+      username: '',
+      password: '',
+      confirmPassord: '',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  height: 54px;
+  width: 182px;
+}
+
+.sign-in-button {
+  height: 44px;
+  border-radius: 5px;
+  background-color: $instablue;
+}
+
+.facebook-icon {
+  height: 17px;
+  width: 17px;
+}
+
+.link {
+  text-decoration: none;
+  color: $instablue;
+}
+
+.separator {
+  width: 35%;
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,9 +4,11 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: 'signin', component: () => import('pages/SignIn/Index.vue'), alias: '' },
+      { path: 'signup', component: () => import('pages/SignUp/Index.vue') },
       { path: 'main', component: () => import('pages/Main/Index.vue ') },
       { path: 'my-area', component: () => import('pages/MyArea/Index.vue ') },
       { path: 'profile', component: () => import('pages/Profile/Index.vue ') },
+      { path: 'new-post', component: () => import('pages/NewPost/Index.vue ') },
     ],
   },
 
